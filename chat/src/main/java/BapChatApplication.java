bb04e667f0d6e4878ee7ec530a27c1683f4e1ff3
Jack Tan <jacktan165@gmail.com>
2018-11-26 00:00:59 +1300

    Added application for chat & dota

---
+++ chat/src/main/java/BapChatApplication.java
@@ -0,0 +1,5 @@
+public class BapChatApplication {
+    public static void main(String[] args) {
+        System.out.println("Chat");
+    }
+}
---
+++ dota/src/main/java/BapDotaApplication.java
@@ -0,0 +1,5 @@
+public class BapDotaApplication {
+    public static void main(String[] args) {
+        System.out.println("Dota");
+    }
+}
