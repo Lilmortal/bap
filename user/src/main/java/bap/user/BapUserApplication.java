737fab832a6774d1da7698f5f28f03455b04f818
Jack Tan <jacktan165@gmail.com>
2018-12-05 00:48:20 +1300

    Added docker compose

--- user/src/main/java/bap/user/BapUserApplication.java
+++ user/src/main/java/bap/user/BapUserApplication.java
@@ -6,14 +6,36 @@ import bap.user.dao.UserDao;
 import bap.user.dao.UserDaoImpl;
 import bap.user.services.UserService;
 import bap.user.services.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
 
 public class BapUserApplication {
-    public static void main(String[] args) {
+    public final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
-        UserDao userDao = new UserDaoImpl();
+    private void init() {
+        UserDao userDao = null;
+
+        // TODO: Put in env
+        String url = "url";
+        String user = "user";
+        String password = "password";
+
+        try {
+            userDao = new UserDaoImpl(url, user, password);
+        } catch (SQLException e) {
+            LOG.error("Failed to connect to database.");
+            LOG.error(e.getSQLState());
+        }
         UserService userService = new UserServiceImpl(userDao);
         UserApiImpl userApiImpl = new UserApiImpl(userService);
 
         new UserController(userApiImpl).start();
+    }
+
+    public static void main(String[] args) {
+        BapUserApplication application = new BapUserApplication();
+        application.init();
     }
 }
